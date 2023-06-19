fe4f10e08da8770b3ad7c2d73f07891f733e9943
arif3ana <ariftriana28@gmail.com>
2023-06-19 09:58:13 +0700

    feat: ..

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -115,6 +115,7 @@ export default function Login({ status, canResetPassword }) {
                                 Forgot your password?
                             </Link>
                         )} */}
+                        {/* Not needed yet */}
                         <Link
                             href={route("register")}
                             className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
